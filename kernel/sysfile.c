6469af50e532649a95d568189fbd0ded4851b5c9
wunaiqiezixin <1947685764@qq.com>
2026-09-24 16:02:26 +0800

    lab 9: Symbolic links(moderate)

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -304,11 +304,31 @@ sys_open(void)
       return -1;
     }
   } else {
-    if((ip = namei(path)) == 0){
-      end_op();
-      return -1;
+    int symlink_depth = 0;
+    for (;;) {
+      if ((ip = namei(path)) == 0) {
+        end_op();
+        return -1;
+      }
+
+      ilock(ip);
+
+      if (ip->type == T_SYMLINK && !(omode == O_NOFOLLOW)) {
+        if (++symlink_depth > 10) {
+          iunlockput(ip);
+          end_op();
+          return -1;
+        }
+        if (readi(ip, 0, (uint64)path, 0, MAXPATH) < 0) {
+          iunlockput(ip);
+          end_op();
+          return -1;
+        }
+      }
+      else
+        break;
+      iunlockput(ip);
     }
-    ilock(ip);
     if(ip->type == T_DIR && omode != O_RDONLY){
       iunlockput(ip);
       end_op();
@@ -393,7 +413,7 @@ sys_chdir(void)
   char path[MAXPATH];
   struct inode *ip;
   struct proc *p = myproc();
-  
+
   begin_op();
   if(argstr(0, path, MAXPATH) < 0 || (ip = namei(path)) == 0){
     end_op();
@@ -482,5 +502,34 @@ sys_pipe(void)
     fileclose(wf);
     return -1;
   }
+  return 0;
+}
+
+uint64
+sys_symlink(void)
+{
+  struct inode* ip;
+  char target[MAXPATH], path[MAXPATH];
+
+  if (argstr(0, target, MAXPATH) < 0 || argstr(1, path, MAXPATH) < 0)
+    return -1;
+
+  begin_op();
+
+  if ((ip = create(path, T_SYMLINK, 0, 0)) == 0) {
+    end_op();
+    return -1;
+  }
+
+  if (writei(ip, 0, (uint64)target, 0, sizeof(target)) < 0) {
+    iunlockput(ip);
+    end_op();
+    return -1;
+  }
+
+  iunlockput(ip);
+
+  end_op();
+
   return 0;
 }
